93445525bb8be8438fbdf633f8acb1e17c767ba6
Akbar <akbar.ak219@gmail.com>
2023-10-27 21:44:11 +0530

    Updated cart-implementation logic till emptying items

--- ui.apps/src/main/content/jcr_root/apps/akart-ecommerce/components/content/cart/site/js/cart.js
+++ ui.apps/src/main/content/jcr_root/apps/akart-ecommerce/components/content/cart/site/js/cart.js
@@ -13,6 +13,15 @@ Handlebars.registerHelper('multiply', function (val1, val2) {
 const cart = JSON.parse(localStorage.getItem('cart'));
 $('#cart-count').text(cart.length);
 
+$(document).on('readystatechange', function (e) {
+    if (e.target.readyState === 'complete') {
+        if ($(".cart-items-wrapper .product").length <= 0) {
+            $(".cart-price-wrapper").remove();
+            $(".cart-items-wrapper").html(`<div class="empty-cart-wrapper"><img src="/content/dam/akart-ecommerce/images/empty-cart.png" class="empty-cart-image"><div class="empty-cart-message">Ohh no! Your cart is empty!!</div></div>`);
+        }
+    }
+});
+
 $.ajax({
     url: '/bin/akart/cart?cartItems=' + JSON.stringify(cart),
     type: 'GET',
@@ -24,14 +33,65 @@ $.ajax({
         var html = template(response);
         $("#cart-items").html(html);
 
+        var totalMrpPrice = 0;
         var totalCart = 0;
-        $(".items-total-price").each(function (itemTotalPrice) {
-            totalCart = eval(totalCart + Number.parseInt($(this).text()));
-        });        
-        $(".cart-price").text(totalCart);
-        $(".total-amount").text(totalCart - $(".discount-price").text());
+        for (var key in response) {
+            if (response.hasOwnProperty(key)) {
+                totalMrpPrice = totalMrpPrice + (response[key].itemCount * response[key].prodMrpPrice);
+                totalCart = totalCart + (response[key].itemCount * response[key].prodPrice);
+            }
+        }
+        $(".cart-price").text(totalMrpPrice);
+        $(".discount-price").text(totalMrpPrice - totalCart);
+        $(".total-amount").text(totalCart);
+        $(".save-message").html(`You will save <span class="currency-inr">${totalMrpPrice - totalCart}</span> on this order`);
     },
     error: function () {
 
     }
-})
+});
+
+$(document).on("click", ".remove-cart-item", function (e) {
+
+    // removing cart item
+    var productPagePath = '';
+    var productDiv = $(this).parents(".product");
+    productDiv.addClass('product-delete');
+    setTimeout(function () {
+        productDiv.remove();
+        updateOrderSummary();
+        var thisProductId = productDiv.find('.product-details .product-id').text();
+        var currentCart = JSON.parse(localStorage.getItem('cart'));
+        var existingCartItem = currentCart.find(item => item.productId == thisProductId);
+        currentCart.splice(existingCartItem, 1);
+        localStorage.setItem('cart', JSON.stringify(currentCart)); // updating cart after removing item
+
+        $('#cart-count').text(JSON.parse(localStorage.getItem('cart')).length);
+
+        if ($(".cart-items-wrapper .product").length <= 0) {
+            $(".cart-price-wrapper").remove();
+            $(".cart-items-wrapper").html(`<div class="empty-cart-wrapper"><img src="/content/dam/akart-ecommerce/images/empty-cart.png" class="empty-cart-image"><div class="empty-cart-message">Ohh no! Your cart is empty!!</div></div>`);
+        }
+    }, 250);
+
+
+});
+
+function updateOrderSummary() {
+    var totalMrpPrice = 0;
+    $(".product-mrpprice").each(function () {
+        var itemQty = $(this).parents('.product').find('.item-count').text();
+        totalMrpPrice = eval(totalMrpPrice + Number.parseInt($(this).text() * itemQty));
+    });
+    $(".cart-price").text(totalMrpPrice);
+
+    var totalCart = 0;
+    $(".items-total-price").each(function () {
+        totalCart = eval(totalCart + Number.parseInt($(this).text()));
+    });
+
+    var discountPrice = totalMrpPrice - totalCart;
+    $(".discount-price").text(discountPrice);
+    $(".total-amount").text(totalCart);
+    $(".save-message").html(`You will save <span class="currency-inr">${totalMrpPrice - totalCart}</span> on this order`);
+}
--- ui.apps/src/main/content/jcr_root/apps/akart-ecommerce/components/content/productslist/clientlibs/site/js/addtocart.js
+++ ui.apps/src/main/content/jcr_root/apps/akart-ecommerce/components/content/productslist/clientlibs/site/js/addtocart.js
@@ -2,6 +2,34 @@ const cart = JSON.parse(localStorage.getItem('cart'));
 let savedCartCount = cart != undefined ? cart.length : 0;
 $('#cart-count').text(savedCartCount);
 
+$(document).on('readystatechange',function(e){
+    if(e.target.readyState === 'complete'){
+        $(".product-list .product").find(".product-id").each(function (params) {
+            var thisProductId = $(this).text();
+            console.log('product id: ', thisProductId);
+            var existingCartItem = cart.find(item => item.productId == thisProductId);
+            console.log('cart item: ', existingCartItem);
+            updateCart1($(this), existingCartItem);
+        });
+    }
+});
+
+function updateCart1(thisButton, item) {
+    console.log('updating for ', item);
+    // Update the cart buttons
+    if (item && item.quantity > 0) {
+        thisButton.parents('.product').find('.edit-product').addClass('add-to-cart-button');
+        thisButton.parents('.product').find('.edit-product').html(`<button class="minus-button" onclick="decrementCart(this, event)">-</button><span class="item-count" style="padding: 10px;">${item.quantity}</span><button class="plus-button" onclick="incrementCart(this, event)" ${item.quantity === 10 ? 'disabled' : ''}>+</button>`);
+        // thisButton.parents('.product').find('.add-to-cart-button').html(`<button class="minus-button" onclick="decrementCart(this, event)">-</button><span class="item-count" style="padding: 10px;">${item.quantity}</span><button class="plus-button" onclick="incrementCart(this, event)">+</button>`);
+    } else {
+        thisButton.parents('.product').find('.edit-product').html(`<button class="add-to-cart-button button">Add to Cart</button>`);
+    }
+
+    if (cart.length == 0) {
+        thisButton.parents('.product').find('.edit-product').html(`<button class="add-to-cart-button button">Add to Cart</button>`);
+    }
+}
+
 //const cart = [];
 
 $(document).on("click", ".add-to-cart-button", function () {
@@ -68,17 +96,22 @@ function decrementCart(thisButton, event) {
 
 function updateCart(thisButton, item) {
     // Update the cart buttons
-    if (item && item.quantity > 0) {
-        thisButton.html(`<button class="cart-button" onclick="decrementCart(this, event)">-</button><span>${item.quantity}</span><button class="cart-button" onclick="incrementCart(this, event)">+</button>`);
-        // $(thisButton).css('display', 'none');
-        // $(thisButton).siblings(".cart-decr-incr-buttons").css('display', 'block');
-        // $(thisButton).siblings(".cart-decr-incr-buttons").find("span.item-count").text(item.quantity)
+    if (item && item.quantity > 1) {
+        thisButton.html(`<button class="minus-button" onclick="decrementCart(this, event)">-</button><span class="item-count" style="padding: 10px;">${item.quantity}</span><button class="plus-button" onclick="incrementCart(this, event)" ${item.quantity === 10 ? 'disabled' : ''}>+</button>`);
+    } else if (item && item.quantity == 1) {
+        thisButton.parents('.edit-product').addClass('add-to-cart-button');
+        thisButton.parents('.edit-product').html(`<button class="minus-button" onclick="decrementCart(this, event)">-</button><span class="item-count" style="padding: 10px;">${item.quantity}</span><button class="plus-button" onclick="incrementCart(this, event)" ${item.quantity === 10 ? 'disabled' : ''}>+</button>`);
+
+        if(thisButton.hasClass('add-to-cart-button')) {
+            thisButton.html(`<button class="minus-button" onclick="decrementCart(this, event)">-</button><span class="item-count" style="padding: 10px;">${item.quantity}</span><button class="plus-button" onclick="incrementCart(this, event)" ${item.quantity === 10 ? 'disabled' : ''}>+</button>`);
+        }
     } else {
-        thisButton.text(`Add to Cart`);
+        thisButton.removeClass('add-to-cart-button');
+        thisButton.html(`<button class="add-to-cart-button button">Add to Cart</button>`);
     }
 
     if (cart.length == 0) {
-        thisButton.text(`Add to Cart`);
+        thisButton.text(`<button class="add-to-cart-button button">Add to Cart</button>`);
     }
     console.log('cart ', cart);
     localStorage.setItem('cart', JSON.stringify(cart));
